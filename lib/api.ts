ea0489732787a670aed81315410b913bf6100aa1
Mats Tuovinen <mats.tuovinen@gmail.com>
2021-06-06 23:13:56 +0200

    Add example homepage.

--- lib/api.ts
+++ lib/api.ts
@@ -3,6 +3,7 @@ import { join } from 'path'
 import matter from 'gray-matter'
 
 const postsDirectory = join(process.cwd(), '_posts')
+const pagesDirectory = join(process.cwd(), '_pages')
 
 export function getPostSlugs() {
   return fs.readdirSync(postsDirectory)
@@ -51,4 +52,46 @@ export function getAllPosts(fields: string[] = []) {
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
   return posts
+}
+
+export function getPageBySlug(slug: string, fields: string[] = []) {
+  const realSlug = slug.replace(/\.md$/, '')
+  const fullPath = join(pagesDirectory, `${realSlug}.md`)
+
+  if (! fs.existsSync(fullPath)) {
+    return null;
+  }
+
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+  const { data, content } = matter(fileContents)
+
+  type Items = {
+    [key: string]: string
+  }
+
+  const items: Items = {}
+
+  // Ensure only the minimal needed data is exposed
+  fields.forEach((field) => {
+    if (field === 'slug') {
+      items[field] = realSlug
+    }
+    if (field === 'content') {
+      items[field] = content
+    }
+
+    if (data[field]) {
+
+      // Make date-object work with gray-matter when
+      if (field === 'date' && typeof data[field] !== 'string' ) {
+        const modified = JSON.stringify(data[field])
+        items[field] = JSON.parse(modified);
+      } else {
+        items[field] = data[field]
+      }
+    }
+  })
+
+  return items
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,37 +3,40 @@ import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
+import { getAllPosts, getPageBySlug } from '../lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 import Post from '../types/post'
 
 type Props = {
+  homePage: Post,
   allPosts: Post[]
 }
 
-const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+const Index = ({ homePage, allPosts }: Props) => {
+  // const heroPost = allPosts[0]
+  // const morePosts = allPosts.slice(1)
+  const title = homePage?.title || 'Home'
+
   return (
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>{title} | Next.js Blog Example with {CMS_NAME}</title>
         </Head>
         <Container>
-          <Intro />
-          {heroPost && (
+          <Intro title={homePage?.title} />
+          {homePage && (
             <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
+              title={homePage.title}
+              coverImage={homePage.coverImage}
+              date={homePage.date}
+              author={homePage.author}
+              slug={homePage.slug}
+              excerpt={homePage.excerpt}
             />
           )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {allPosts.length > 0 && <MoreStories posts={allPosts} />}
         </Container>
       </Layout>
     </>
@@ -43,6 +46,18 @@ const Index = ({ allPosts }: Props) => {
 export default Index
 
 export const getStaticProps = async () => {
+  const homePage = getPageBySlug(
+    'home',
+    [
+      'title',
+      'date',
+      'slug',
+      'author',
+      'coverImage',
+      'excerpt',
+    ]
+  )
+
   const allPosts = getAllPosts([
     'title',
     'date',
@@ -53,6 +68,6 @@ export const getStaticProps = async () => {
   ])
 
   return {
-    props: { allPosts },
+    props: { homePage, allPosts },
   }
 }
